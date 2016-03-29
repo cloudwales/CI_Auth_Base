b66b3341c0a91c830e4c7d3564996c5611f56225
Chris Bowen <info@cloud-wales.co.uk>
2016-03-29 19:43:42 +0100

    Added jamierumbelow/codeigniter-base-model

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'MY_Model' => $vendorDir . '/jamierumbelow/codeigniter-base-model/core/MY_Model.php',
 );
