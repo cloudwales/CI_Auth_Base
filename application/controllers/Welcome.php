989b358d0463fccc87dc069e01fbad5e5bb81558
Chris Bowen <info@cloud-wales.co.uk>
2016-03-30 20:31:33 +0100

    added bootstrap and .htaccess

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/header');
+		$this->load->view('main_page');
+		$this->load->view('template/footer');
 	}
 }
