e653e3160d85dac399f7e06de86c331758fe47e3
Chris Bowen <info@cloud-wales.co.uk>
2016-03-30 20:33:29 +0100

    Added Key and sessions to database

--- application/config/config.php
+++ application/config/config.php
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '4RKJAPHzxMqezbLOY4nlrzagCxJ06QWu';
 
 /*
 |--------------------------------------------------------------------------
@@ -367,7 +367,7 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
