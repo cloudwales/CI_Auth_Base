b625b6f7f95f62209dbc93dd0b891a0b3985b394
Chris Bowen <info@cloud-wales.co.uk>
2016-03-30 21:39:44 +0100

    Restarted the files to work in the environment

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,9 +11,8 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		echo ENVIRONMENT; 
-		// $this->load->view('template/header');
-		// $this->load->view('main_page');
-		// $this->load->view('template/footer');
+		$this->load->view('template/header');
+		$this->load->view('main_page');
+		$this->load->view('template/footer');
 	}
 }
