98e77f9264dd9ecdb65fa43c1f00c8fec94114e0
Chris Bowen <info@cloud-wales.co.uk>
2016-04-04 16:43:22 +0100

    Fixed menu in dashboard

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -48,12 +48,9 @@
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Settings <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="#">My Profile</a></li>
-                    <li role="separator" class="divider"></li>
                     <li><a href="<?php echo base_url('auth/index'); ?>">View Users</a></li>
                     <li><a href="<?php echo base_url('auth/create_user'); ?>">Create User</a></li>
                     <li role="separator" class="divider"></li>
-                    <li><a href="#">System Settings</a></li>
                 </ul>
             </li>     
             <li><a href="<?php echo base_url('auth/logout'); ?>">Logout</a></li>
